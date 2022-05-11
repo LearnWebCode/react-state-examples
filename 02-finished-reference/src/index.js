3ce3c50137e01a5fe80667f0591019dedc73deb5
Brad <brad@learnwebcode.com>
2022-05-11 15:59:06 -0700

    Changes.

--- 02-finished-reference/src/index.js
+++ 02-finished-reference/src/index.js
@@ -2,11 +2,15 @@ import React, { useState } from "react"
 import { createRoot } from "react-dom/client"
 
 function App() {
-  const [size, setSize] = useState(14)
+  const [size, setSize] = useState(25)
   const [color, setColor] = useState("skyblue")
-  const [likes, setLikes] = useState(3)
+  const [likeCount, setLikeCount] = useState(0)
 
-  function ourCustomHandler() {
+  function handleSizeChange(e) {
+    setSize(e.target.value)
+  }
+
+  function ourButtonHandler() {
     setSize(20)
     setColor("pink")
   }
@@ -14,20 +18,20 @@ function App() {
   return (
     <div className="grid-parent">
       <div className="header">
-        <h1>Welcome To Our App</h1>
+        <h1>Welcome To Our App!!!</h1>
         <p>
           The current size is {size} and the current color is {color}.
         </p>
         <p>
-          This page has been liked <strong>{likes}</strong> times.
+          This page has been liked <strong>{likeCount}</strong> times.
         </p>
       </div>
       <div className="sidebar">
-        <input type="text" onChange={e => setColor(e.target.value)} value={color} />
-        <input type="text" onChange={e => setSize(e.target.value)} value={size} />
-        <button onClick={ourCustomHandler}>Make the text 20px and pink</button>
+        <input type="text" value={size} onChange={handleSizeChange} />
+        <input type="text" value={color} onChange={e => setColor(e.target.value)} />
+        <button onClick={ourButtonHandler}>Make the text 20px and pink</button>
       </div>
-      <div className="main-area" style={{ color: color, fontSize: `${size}px` }}>
+      <div className="main-area" style={{ color, fontSize: `${size}px` }}>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, quod obcaecati dolore debitis amet aut, excepturi voluptas ratione quibusdam laboriosam, optio pariatur provident doloremque consequatur animi? Consequuntur, officiis. Explicabo, vel.</p>
       </div>
       <footer className="footer">
@@ -35,7 +39,7 @@ function App() {
           This is the footer. <button onClick={() => setSize(30)}>Make the text 30px but leave the color the same</button>
         </p>
         <p>
-          <button onClick={() => setLikes(prev => prev + 1)}>Like The Page</button>
+          <button onClick={() => setLikeCount(prev => prev + 1)}>Like The Page</button>
         </p>
       </footer>
     </div>
